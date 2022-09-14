9a626ad8d850d6aa38d00d4b4fddca14c55c1c00
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2022-09-14 12:32:59 +0300

    fix: header__nav height on scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const header__nav = document.querySelector(".header__nav")
+const header__nav = document.querySelector(".header__nav");
+const logo = document.querySelector(".logo__svg use");
 
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1) {
     header__nav.classList.add("nav-white");
+    logo.href.baseVal = "img/sprite.svg#logo_black";
   }
   else {
     header__nav.classList.remove("nav-white");
+    logo.href.baseVal = "img/sprite.svg#logo_white";
   }
-});
+}); 
